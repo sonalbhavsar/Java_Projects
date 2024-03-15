ac5e65fb68f0e8e68df68493537a98de48ee3ce9
sonalbhavsar <134159290+sonalbhavsar@users.noreply.github.com>
2024-03-15 19:22:00 +0530

    Create Coordinate.java

---
+++ Snake_Ladder_Game/coordinate/Coordinate.java
@@ -0,0 +1,25 @@
+package coordinate;
+
+public class Coordinate {
+    int row;
+    int col;
+
+    public Coordinate(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public String printCoordinate() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(this.row).append(",").append(this.col).append("]");
+        return sb.toString();
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getColValue() {
+        return this.col;
+    }
+}
